f009d3a7c4020b861778018d27c403b4d7a71185
eddah <eddahbaty98@gmail.com>
2020-08-24 10:05:51 +0300

    add  a button to toggle between details

--- Documents/CORE/quotes/src/app/quote.ts
+++ Documents/CORE/quotes/src/app/quote.ts
@@ -1,2 +1,12 @@
 export class Quote {
+    showDescription: boolean;
+    constructor(
+         public id: number,
+         public name: string, 
+         public author: string, 
+         public blogger: string, 
+         public postingDate: Date
+         ) {
+        this.showDescription = false;
+    }
 }
--- Documents/CORE/quotes/src/app/quote/quote.component.ts
+++ Documents/CORE/quotes/src/app/quote/quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quote[] = [
+    new Quote(1, 'It is impossible to live without failing at something, unless you live so cautiously that you might as well not have lived at all - in which case, you fail by default.', 
+    'J.K Rowlings', 'Mrs. Bati', new Date(2020,8,23)),
+    new Quote(2,'You may encounter many defeats, but you must not be defeated.In fact, it may be necessary to encounter the defeats, so you can know who you are, what you can rise from, how you can still come out of it.',
+    'Maya Angelou', 'Chebet Edda', new Date(2020,8,20)), 
+    
+  ];
+  toggleDetails(index) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
 
   constructor() { }
 
